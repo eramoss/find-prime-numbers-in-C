6cd6d65dd93d77fa0137357051c59c7b192e597d
Eduardo Ramos <101826448+eramoss@users.noreply.github.com>
2023-02-28 11:27:02 -0300

    format document

--- prime-numbers.c
+++ prime-numbers.c
@@ -1,45 +1,55 @@
 #include <stdbool.h>
 #include <stdio.h>
-#define FIRST_POSITION_AFTER_FIVE  3
-#define LAST_VALUE_OF_PREALLOCATED_ARRAY  5
+#define FIRST_POSITION_AFTER_FIVE 3
+#define LAST_VALUE_OF_PREALLOCATED_ARRAY 5
 #define ARRAY_SIZE 1000
 #define MAX_PRIME_NUMBERS 1000
-#define FIRSTS_OF_PRIME_NUMBERS 2,3,5
+#define FIRSTS_OF_PRIME_NUMBERS 2, 3, 5
 
 int allPrimeNumbers[ARRAY_SIZE] = {FIRSTS_OF_PRIME_NUMBERS};
 int nextValue = LAST_VALUE_OF_PREALLOCATED_ARRAY;
 int position = FIRST_POSITION_AFTER_FIVE;
 
-bool isPrime(int value, int position) {
-  
-  while (position >= 0) {
-    if (value % allPrimeNumbers[position] == 0) {
-      return false;
-    } 
+bool isPrime(int value, int position)
+{
+
+  while (position >= 0)
+  {
+    if (allPrimeNumbers[position] != 0)
+    {
+
+      if (value % allPrimeNumbers[position] == 0)
+      {
+
+        return false;
+      }
+    }
     position--;
   }
+
   return true;
 }
 
-bool  NextNumber() {
-  
-  
-  if (isPrime(nextValue, position-1)) {
+bool NextNumber()
+{
+
+  if (isPrime(nextValue, position - 1))
+  {
     allPrimeNumbers[position] = nextValue;
-    printf("%d\n",allPrimeNumbers[position]);
   }
-  
+
   position++;
 }
 
-void main() {
-  
+void main()
+{
 
-  for (nextValue; nextValue < MAX_PRIME_NUMBERS; nextValue++) {
+  for (nextValue; nextValue < MAX_PRIME_NUMBERS; nextValue = nextValue + 2)
+  {
+
     NextNumber();
   }
-  
-  
 
   return;
 };
+
